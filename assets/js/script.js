c7aa73a5f79c78e57ec51419e14441d884328f68
Kiyas-Mohamed <darkdoor77@gmail.com>
2024-06-08 12:17:18 +0530

    Initial Commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+window.dataLayer = window.dataLayer || [];
+function gtag() { dataLayer.push(arguments); }
+gtag('js', new Date());
+gtag('config', 'G-R2KH9XM5ZY');
+
+
+var navLinks = document.getElementById("navLinks"); {
+    function showMenu() {
+        navLinks.style.right = "0";
+    }
+    function hideMenu() {
+        navLinks.style.right = "-200px";
+    }
+}
